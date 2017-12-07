92473584821a52ceeedf0e43c56083ebae1d89f2
Liz <wanderlust3x3@gmail.com>
2017-12-06 23:00:55 -0500

    Add project files.

---
+++ IRL.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IRL.Web.Startup))]
+namespace IRL.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
